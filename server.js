7e7d6e2a926de30c950f12d5b31e259df44c5ac0
Jahblessme24 <Jahblessme002@gmail.com>
2024-12-25 08:16:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(cors());
 const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
-const userEmail = "destinyjunior75@gmail.com";
-const pass = "julpwzidgltgcjri";
-// real 15 nov
+const userEmail = "Jahblessme002@gmail.com";
+const pass = "dhrsmrwftoxiucwo";
+// 25 jan
 
 // Middleware
 app.use(express.json());
